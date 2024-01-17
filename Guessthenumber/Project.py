1b1d1dc3de553ef856061507ffd0ca04ac377b7e
Snigdha1808 <141983332+Snigdha1808@users.noreply.github.com>
2024-01-17 11:50:14 +0530

    Update Project.py

--- Guessthenumber/Project.py
+++ Guessthenumber/Project.py
@@ -1,27 +1,29 @@
 import random as r
-n=r.randrange(100)
-x=5
-print("Enter the numbers within the range 0 and 100")
-while x>=0:
-    print("Enter your guessed number")
-    guess=int(input())
-    def check(y):
-        if guess==y:
-            print("You've guessed correctly. Congratulations!!")
-            return True
-        elif guess>y:
-            print("Try to guess a little lower")
-        else:
-            print("Try to guess a little higher")
-            return False
-    if x>1:
-        if check(n):
-            break
-    elif x==1:
-        if check(n):
-            break
-        print("This is your last chance")
+
+n = r.randrange(100)
+x = 5
+
+print("Enter the number in the range of 1 to 100")
+
+for i in range(x):
+    if i == x - 1:
+        print("This is your last chance.")
+        print("Enter your guessed number")
+        guess = int(input())
+
+        if guess != n:
+            print("Sorry. You lost the game")
+            print("The number is: ", n)
+        elif guess == n:
+            print("You guessed the number correctly. CONGRATULATIONS!!")
     else:
-        print("Sorry. You lost!")
-        print("The number is: ",n)
-    x=x-1
+        print("Enter your guessed number")
+        guess = int(input())
+
+        if guess == n:
+            print("You guessed the number correctly. CONGRATULATIONS!!")
+            break
+        elif guess > n:
+            print("Try guessing a little lower")
+        else:
+            print("Try guessing a little higher")
